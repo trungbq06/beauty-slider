4bb773e9f752b6ba11bab52d03e55a713496c05b
CPU0043\TrungBQ <trungbq06@gmail.com>
2015-12-30 17:52:28 +0700

    add source files

---
+++ js/jquery.beauty-slider.js
@@ -0,0 +1,24 @@
+/*!
+ * Beauty Slider jQuery plugin
+ * http://add_address_here
+ *
+ * Copyright 2015, Trung Bui
+ *
+ * Date: Wed Dec 30 16:52:21 2015
+ */
+
+(function ( $ ) {
+ 
+    $.fn.btSlider = function( options ) {
+ 
+        // Default options
+        var settings = $.extend({
+            speed: 0,
+            pager: true,
+            controls: true,
+        }, options );
+
+
+    };
+ 
+}( jQuery ));
