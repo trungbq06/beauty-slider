2b9be6a87dbcd702db8128228b3653d51316f325
CPU0043\TrungBQ <trungbq06@gmail.com>
2015-12-31 17:25:41 +0700

    - add options

--- js/jquery.beauty-slider.js
+++ js/jquery.beauty-slider.js
@@ -8,17 +8,41 @@
  */
 
 (function ( $ ) {
- 
-    $.fn.btSlider = function( options ) {
- 
-        // Default options
-        var settings = $.extend({
-            speed: 0,
-            pager: true,
-            controls: true,
-        }, options );
 
+  // Default options
+  var defaults = {
+      debug: true,
+      speed: 0,
+      pager: true,
+      controls: true,
+      auto: true,
+      pause: 4000
+  };
 
-    };
+  $.fn.btSlider = function( options ) {
+    if (this.length === 0) {
+      return this;
+    }
+
+    // loop through elements
+    if (this.length > 1) {
+      this.each(function() {
+        this.btSlider(options);
+      });
+
+      return this;
+    }
+
+    var slider = {};
+
+    var element = $(this);
+
+    // Get the window width and height
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+
+    // Check if slider is already initialized
+    if (element.data('btSlider')) { return; }
+  };
  
 }( jQuery ));
